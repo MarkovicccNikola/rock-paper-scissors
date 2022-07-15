42d206cf7bd6441ae3953a334a5c3caedcb9f6c5
Nikola Markovic <markovi3c.nikola@gmail.com>
2022-07-15 17:02:48 +0200

    Finish rock-paper-scissors in console

---
+++ script.js
@@ -0,0 +1,71 @@
+let computer;
+let computerSelection;
+let humanSelection;
+let result;
+let humanChoice;
+let humanScore = 0;
+let computerScore = 0;
+let resultHumanWins;
+let resultTie;
+let tie = 0;
+let humanWins = 0;
+let computerWins = 0;
+
+function humanPlay() {
+  humanChoice = prompt();
+  humanSelection = humanChoice.trim().toLowerCase();
+}
+
+function computerPlay() {
+  computer = Math.floor(Math.random() * 3 + 1);
+  computerSelection =
+    computer === 1 ? "rock" : computer === 2 ? "paper" : "scissors";
+}
+
+function round(computerSelection, humanSelection) {
+  result =
+    humanSelection === computerSelection
+      ? `human: ${humanSelection}\ncomputer: ${computerSelection}\ntie game`
+      : humanSelection === "rock" && computerSelection === "paper"
+      ? `human: ${humanSelection}\ncomputer: ${computerSelection}\ncomputer wins`
+      : humanSelection === "scissors" && computerSelection === "rock"
+      ? `human: ${humanSelection}\ncomputer: ${computerSelection}\ncomputer wins`
+      : humanSelection === "paper" && computerSelection === "scissors"
+      ? `human: ${humanSelection}\ncomputer: ${computerSelection}\ncomputer wins`
+      : `human: ${humanSelection}\ncomputer: ${computerSelection}\nhuman wins`;
+  resultHumanWins = result.includes("human wins");
+  resultTie = result.includes("tie game");
+
+  console.log(result);
+}
+
+function play() {
+  humanPlay();
+  computerPlay();
+  round(computerSelection, humanSelection);
+}
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    humanPlay();
+    computerPlay();
+    round(computerSelection, humanSelection);
+
+    if (resultTie) {
+      ++tie;
+    } else if (resultHumanWins) {
+      ++humanWins;
+    } else {
+      ++computerWins;
+    }
+  }
+  if (humanWins === computerWins) {
+    console.log("It's a tie!!!");
+  } else if (humanWins > computerWins) {
+    console.log("SUCK IT, AI!");
+  } else {
+    console.log("AI is just too lucky...");
+  }
+}
+
+game();
